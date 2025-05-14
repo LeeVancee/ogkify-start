552e4ad06f3b026bdd34dea89ee2076415fcf97e
LEEVANCEE <1280403599@qq.com>
2025-05-14 09:35:39 +0800

    change prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../../generated/prisma'
+import 'dotenv/config'
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 export const prisma = new PrismaClient()
