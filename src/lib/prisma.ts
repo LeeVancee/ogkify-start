88e734cc504eb9db484bdc48f2d616cc0c12fff6
LEEVANCEE <1280403599@qq.com>
2025-05-25 10:12:28 +0800

    remove prisma adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,3 @@
-import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../generated/prisma'
-import 'dotenv/config'
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 export const prisma = new PrismaClient()
