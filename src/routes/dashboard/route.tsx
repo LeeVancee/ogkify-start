becb34ac6ebb20ddc861626b2a2cfcb68b4cfbf6
LEEVANCEE <1280403599@qq.com>
2026-05-04 20:40:39 +0800

    context

--- src/routes/dashboard/route.tsx
+++ src/routes/dashboard/route.tsx
@@ -3,17 +3,13 @@ import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 import { DashboardHeader } from "@/components/dashboard/layout/header";
 import { DashboardSidebar } from "@/components/dashboard/layout/sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { getSession } from "@/server/getSession";
 
 export const Route = createFileRoute("/dashboard")({
-  beforeLoad: async () => {
-    const session = await getSession();
-
-    if (!session) {
-      throw redirect({ to: "/login" });
-    }
-
-    if (session.user.role !== "admin") {
+ beforeLoad: ({ context }) => {
+  if(!context.session){
+    throw redirect({ to: "/login" });
+  }
+    if (context.session.user.role !== "admin") {
       throw redirect({ to: "/" });
     }
   },
